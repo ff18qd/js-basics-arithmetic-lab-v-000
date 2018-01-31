08dcdaef1e18cf763fc5ce71758997b8f6d86c30
Emma Feng <ff18qd@gmail.com>
2018-01-31 19:25:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ const ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random() * 20;
 let randomInteger = Math.floor(randomNumber);
 let randomUserID = 1000000000 + randomInteger;  
+
